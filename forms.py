f2018a0c5ea502e061a109eff154c2b6f4a5a1db
Mustafa Al-Saegh <mustafawm@msn.com>
2016-10-02 18:42:26 +1100

    done

--- forms.py
+++ forms.py
@@ -10,3 +10,14 @@ class SignupForm(Form):
     email = StringField('Email', validators=[DataRequired(), Email('Please enter a valid email address')])
     password = PasswordField('Password', validators=[DataRequired('Please enter a strong password'), Length(min=6, message="Password must be 6 or more charachters long")])
     submit = SubmitField('Sign up')
+
+
+class LoginForm(Form):
+    email = StringField('Email', validators=[DataRequired(), Email('Please enter your email address')])
+    password = PasswordField('Password', validators=[DataRequired('Please enter your password')])
+    submit = SubmitField("Sign in")
+
+
+class AddressForm(Form):
+    address = StringField('Address', validators=[DataRequired('Please enter a valid address')])
+    submit = SubmitField('Search')
--- routes.py
+++ routes.py
@@ -1,8 +1,6 @@
-from flask import Flask, render_template, request
-from models import db, User
-from forms import SignupForm
-
-
+from flask import Flask, render_template, request, session, redirect, url_for
+from models import db, User, Place
+from forms import SignupForm, LoginForm, AddressForm
 
 
 app = Flask(__name__)
@@ -18,6 +16,30 @@ app.secret_key = 'some-secret'
 def index():
     return render_template('index.html')
 
+@app.route('/home', methods=['POST','GET'])
+def home():
+    if 'email' not in session:
+        return redirect(url_for('login'))
+
+    my_coordinates = (37.4221, -122.0844)
+    places = []
+    form = AddressForm()
+    if request.method == 'POST':
+        if form.validate() == False:
+            return render_template('home.html', form=form)
+        else:
+            address = form.address.data
+            p = Place()
+            my_coordinates = p.address_to_latlng(address)
+            places = p.query(address)
+
+            return render_template('home.html',form=form, my_coordinates=my_coordinates, places=places)
+
+    elif request.method == 'GET':
+        return render_template('home.html',form=form, my_coordinates=my_coordinates, places=places)
+
+
+
 
 @app.route('/about')
 def about():
@@ -25,6 +47,9 @@ def about():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if 'email' in session:
+        return redirect(url_for('home'))
+
     form = SignupForm()
     if request.method == 'POST':
         if form.validate() == False:
@@ -33,9 +58,38 @@ def signup():
             newuser = User(form.firstname.data, form.lastname.data, form.email.data, form.password.data)
             db.session.add(newuser)
             db.session.commit()
-            return 'success'
+            session['email'] = newuser.email
+            return redirect(url_for('home'))
     elif request.method == 'GET':
         return render_template('signup.html',form=form)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if 'email' in session:
+        return redirect(url_for('home'))
+
+    form = LoginForm()
+
+    if request.method == 'POST':
+        if form.validate() == False:
+            return render_template('login.html', form=form)
+        else:
+            email = form.email.data
+            password = form.password.data
+
+            user = User.query.filter_by(email=email).first()
+            if user is not None and user.check_password(password):
+                session['email'] = email
+                return redirect(url_for('home'))
+            else:
+                return redirect(url_for('login'))
+    elif request.method == 'GET':
+        return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    session.pop('email', None)
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
